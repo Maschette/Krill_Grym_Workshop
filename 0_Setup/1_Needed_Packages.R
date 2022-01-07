1315cd526f6fe46108f84a9261f44f62d1008a33
Dale Maschette <dale.maschette@aad.gov.au>
2022-01-07 12:59:23 +1100

    Setup Commit

---
+++ 0_Setup/1_Needed_Packages.R
@@ -0,0 +1,19 @@
+#Install needed packages
+install.packages("tidyverse")
+install.packages("remotes")
+install.packages("furrr")
+install.packages("rmarkdown", dependencies = T)
+install.packages("flextable")
+remotes::install_github("AustralianAntarcticDivision/Grym", dependencies = TRUE)
+
+#load needed packages to make sure they worked. 
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(remotes)
+library(purrr)
+library(furrr)
+library(readxl)
+library(Grym)
+library(flextable)
+
